878421ec683d94aee2942406edd20beefd833116
jhonnymalta <jhonatamalta@hotmail.com>
2024-05-21 20:10:08 -0300

    CreateJWTBearerToken Return

---
+++ src/services/MP.Identidade.API/Extensions/AppSetting.cs
@@ -0,0 +1,10 @@
+﻿namespace MP.Identidade.API.Extensions
+{
+    public class AppSetting
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
--- src/services/MP.Identidade.API/Program.cs
+++ src/services/MP.Identidade.API/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MP.Identidade.API.Data;
+using MP.Identidade.API.Extensions;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
 
@@ -14,14 +20,45 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
-
+//jwtbearer
+var appSettingSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSetting>(appSettingSection);
+var appSettings = appSettingSection.Get<AppSetting>();
+var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(bearerOptions =>
+{
+    bearerOptions.RequireHttpsMetadata = true;
+    bearerOptions.SaveToken = true;
+    bearerOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = appSettings.ValidoEm,
+        ValidIssuer = appSettings.Emissor,
+    };
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(name: "v1", new OpenApiInfo
+    {
+        Title = "Médicos Pronto Identity Api",
+        Description = "Esta API faz parte da aplicação enterprise Médicos Pronto",
+        Contact = new OpenApiContact() { Name = "Jhonatan Landes", Email = "jhonatanlandes@gmail.com" },
+        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+    });
+});
 
 var app = builder.Build();
 
@@ -29,7 +66,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "v1");
+    });
 }
 
 app.UseHttpsRedirection();
